e20b4ede13ffa45aecc65b615daf4814fcc5e0a5
Miguel Tablado <miguel.tablado.leon@ibm.com>
2020-02-08 09:56:41 +0100

    TSLint max line to 150

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -14,7 +14,6 @@ export const environment = {
   // server_url: 'https://teg-api.iscp-demo.ibm.com:3443',
   // server_api: 'https://teg-api.iscp-demo.ibm.com:3443/private/api/v1',
   // IBM Roks
-  // tslint:disable-next-line:max-line-length
   server_url: 'https://teg-tracker.ibc02-abc-001-k01-4b190db9285fe795bac33ec62b1ece4d-0001.osl01.containers.appdomain.cloud',
   server_api: 'https://teg-tracker.ibc02-abc-001-k01-4b190db9285fe795bac33ec62b1ece4d-0001.osl01.containers.appdomain.cloud/private/api/v1',
   // GKE
