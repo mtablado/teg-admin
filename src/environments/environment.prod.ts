86035c9a08e67154b0afc0ebf5a0a85d47e70cba
Miguel Tablado <miguel.tablado.leon@ibm.com>
2020-02-08 09:26:12 +0100

    ROKS env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,8 +5,15 @@
  */
 export const environment = {
   production: true,
-  server_url: 'http://35.198.98.50:28080',
-  server_api: 'http://35.198.98.50:28080/private/api/v1',
+  // IBM tst Milan
+  // server_url: 'https://teg-api.iscp-demo.ibm.com:3443',
+  // server_api: 'https://teg-api.iscp-demo.ibm.com:3443/private/api/v1',
+  // IBM Roks
+  server_url: 'https://teg-tracker.ibc02-abc-001-k01-4b190db9285fe795bac33ec62b1ece4d-0001.osl01.containers.appdomain.cloud',
+  server_api: 'https://teg-tracker.ibc02-abc-001-k01-4b190db9285fe795bac33ec62b1ece4d-0001.osl01.containers.appdomain.cloud/private/api/v1',
+  // GKE
+  // server_url: 'http://35.198.98.50:28080',
+  // server_api: 'http://35.198.98.50:28080/private/api/v1',
   refresh_traffic_interval: 20000,
   refresh_drivers_interval: 20000,
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,11 +9,19 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false,
+  production: true,
+  // IBM tst Milan
+  // server_url: 'https://teg-api.iscp-demo.ibm.com:3443',
+  // server_api: 'https://teg-api.iscp-demo.ibm.com:3443/private/api/v1',
+  // IBM Roks
+  server_url: 'https://teg-tracker.ibc02-abc-001-k01-4b190db9285fe795bac33ec62b1ece4d-0001.osl01.containers.appdomain.cloud',
+  server_api: 'https://teg-tracker.ibc02-abc-001-k01-4b190db9285fe795bac33ec62b1ece4d-0001.osl01.containers.appdomain.cloud/private/api/v1',
+  // GKE
+  // server_url: 'http://35.198.98.50:28080',
+  // server_api: 'http://35.198.98.50:28080/private/api/v1',
+  // localhost
   // server_url: 'http://localhost:8080',
   // server_api: 'http://localhost:8080/private/api/v1',
-  server_url: 'http://35.198.98.50:28080',
-  server_api: 'http://35.198.98.50:28080/private/api/v1',
-  refresh_traffic_interval: 5000,
-  refresh_drivers_interval: 5000,
+  refresh_traffic_interval: 20000,
+  refresh_drivers_interval: 20000,
 };
